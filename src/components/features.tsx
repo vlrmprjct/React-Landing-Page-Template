7d3cdbc598b779989a94b9dee542b84a0aaffdc2
Thomas M <mail@lokalnetz.com>
2024-10-01 08:54:27 +0200

    fix features,, cleanup markup

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 
 export const Features = (props) => {
-  return (
-    <div id="features" className="text-center">
-      <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Features</h2>
-        </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : "Loading..."}
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <section id="features" className="container text-center">
+            <h2 className="section-title">Features</h2>
+            <div className="row">
+                {props.data
+                    ? props.data.map((d, i) => (
+                        <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                            <i className={d.icon}></i>
+                            <h3>{d.title}</h3>
+                            <p>{d.text}</p>
+                        </div>
+                    ))
+                    : "Loading..."}
+            </div>
+        </section>
+    );
 };
