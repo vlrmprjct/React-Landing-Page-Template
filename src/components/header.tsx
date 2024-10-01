1444914258657871a49d91efb7d22ba76d86d6d4
Thomas M <mail@lokalnetz.com>
2024-10-01 08:44:03 +0200

    fix header, cleanup markup

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 
 export const Header = (props) => {
-  return (
-    <header id="header">
-      <div className="intro">
-        <img src="assets/img/intro-bg.jpg" />
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
-              </div>
+    return (
+        <header id="header">
+            <div className="overlay">
+                <div className="container intro-text">
+                    <h1>
+                        {props.data ? props.data.title : "Loading"}
+                        <span></span>
+                    </h1>
+                    <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                    <a
+                        href="#features"
+                        className="btn btn-custom btn-lg"
+                    >
+                        Learn More
+                    </a>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+            <img src="assets/img/intro-bg.jpg" />
+        </header>
+    );
 };
