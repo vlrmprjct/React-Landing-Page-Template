bcc8a3cc8df0b6e338c177cdda880af6f9d8c86c
Thomas M <mail@lokalnetz.com>
2024-10-26 12:34:00 +0200

    remove useless loader @ header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-export const Header = (props) => {
+export const Header = ({ data }) => {
     return (
         <header id="header">
-            <div className="intro" style={{ backgroundImage: `url(./assets/img/intro-bg.jpg)` }}>
+            <div className="intro" style={{ backgroundImage: 'url(./assets/img/intro-bg.jpg)' }}>
                 <div className="overlay">
                     <div className="container">
                         <div className="row justify-content-md-center">
                             <div className="intro-text col-md-8">
                                 <h1>
-                                    {props.data ? props.data.title : "Loading"}
+                                    {data.title}
                                 </h1>
-                                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                                <p>{data.paragraph}</p>
                                 <a
                                     href="#features"
                                     className="btn btn-custom btn-lg"
