4f3d6cb53f485d04cf2fe45df6036e37714d993a
Thomas M <mail@lokalnetz.com>
2024-10-26 12:30:01 +0200

    remove broken link @ footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,11 +4,7 @@ export const Footer = () => {
 
     return (
         <footer id="footer" className="container text-center">
-            &copy; 2024 Issaaf Kattan React Land Page Template. Design by
-            {' '}
-            <a href="http://www.templatewire.com" rel="nofollow">
-                TemplateWire
-            </a>
+            &copy; 2024 Issaaf Kattan React Land Page Template.
         </footer>
     );
 };
