23c91437773390d4ec021071c74b2da1ebbb4872
Thomas M <mail@lokalnetz.com>
2024-10-26 12:38:26 +0200

    remove useless loader @ testimonials

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React from 'react';
 
-export const Testimonials = (props) => {
+export const Testimonials = ({ data }) => {
     return (
         <section id="testimonials" className="section-gray">
             <div className="container text-center">
                 <h2 className="section-title">What our clients say</h2>
                 <div className="row">
-                    {props.data
-                        ? props.data.map((d, i) => (
-                            <div key={`${d.name}-${i}`} className="col-md-4">
-                                <div className="testimonial">
-                                    <div className="testimonial-image">
-                                        <img src={d.img} alt="" />
-                                    </div>
-                                    <div className="testimonial-content text-start">
-                                        <p>"{d.text}"</p>
-                                        <div className="testimonial-meta"> - {d.name} </div>
-                                    </div>
+                    {data.map(({ name, img, text }, i) => (
+                        <div key={`${name}-${i}`} className="col-md-4">
+                            <div className="testimonial">
+                                <div className="testimonial-image">
+                                    <img src={img} alt="" />
+                                </div>
+                                <div className="testimonial-content text-start">
+                                    <p>"{text}"</p>
+                                    <div className="testimonial-meta"> - {name} </div>
                                 </div>
                             </div>
-                        ))
-                        : "loading"}
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
