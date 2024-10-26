c6b0227818f998bf61d91bedb3ad822e1618d4b3
Thomas M <mail@lokalnetz.com>
2024-10-26 12:43:43 +0200

    remove useless serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React, { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 import '@/css/index.css';
 
 const root = document.getElementById('root');
@@ -17,8 +16,3 @@ const rootElement = createElement(() => {
 });
 
 createRoot(root).render(rootElement);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
