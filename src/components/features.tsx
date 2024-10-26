4987cd68ec104d5e096378a40d5a77b0218e3eb9
Thomas M <mail@lokalnetz.com>
2024-10-26 12:28:59 +0200

    remove useless loader @ feature

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,20 +1,18 @@
-import React from "react";
+import React from 'react';
 
-export const Features = (props) => {
+export const Features = ({ data }) => {
     return (
         <section id="features" className="section-gray">
             <div className="container text-center">
                 <h2 className="section-title">Features</h2>
                 <div className="row">
-                    {props.data
-                        ? props.data.map((d, i) => (
-                            <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                                <i className={`${d.icon} fa-2x`}></i>
-                                <h3>{d.title}</h3>
-                                <p>{d.text}</p>
-                            </div>
-                        ))
-                        : "Loading..."}
+                    {data.map(({ title, text, icon }, i) => (
+                        <div key={`${title}-${i}`} className="col-xs-6 col-md-3">
+                            <i className={`${icon} fa-2x`}></i>
+                            <h3>{title}</h3>
+                            <p>{text}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
